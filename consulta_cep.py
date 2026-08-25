7be9d917dc03b235709500ef69d5a3165752dd29
PPadilha19 <pedrohenriquepadilhaferreira@gmail.com>
2026-08-25 16:18:32 -0300

    Simplify output to print all response data

--- consulta_cep.py
+++ consulta_cep.py
@@ -4,8 +4,4 @@ cep = "01310930"
 url = f"https://viacep.com.br/ws/{cep}/json/"
 resposta = requests.get(url)
 dados = resposta.json()
-print("CEP:", dados["cep"]) 
-print("Rua:", dados["logradouro"]) 
-print("Bairro:", dados["bairro"]) 
-print("Cidade:", dados["localidade"]) 
-print("Estado:", dados["uf"]) 
+print(dados)
